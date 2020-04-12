86b65f85f4ecc8549c19ab2de2db1456fd1457d6
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-04-12 16:45:31 -0500

    Updates

---
+++ precache-manifest.4ae6f67cf10f20c7a4dbe80a9386734a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583d0f6ac19e25357e3fd7e19b9e5d04",
+    "url": "/mahmoudaosman/index.html"
+  },
+  {
+    "revision": "8e697f07c0c841eddb35",
+    "url": "/mahmoudaosman/static/css/main.5ae536d3.chunk.css"
+  },
+  {
+    "revision": "55b6d95b129cc0a45711",
+    "url": "/mahmoudaosman/static/js/2.b860645f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mahmoudaosman/static/js/2.b860645f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e697f07c0c841eddb35",
+    "url": "/mahmoudaosman/static/js/main.01432343.chunk.js"
+  },
+  {
+    "revision": "16b22d9a09f9f7dbeda5",
+    "url": "/mahmoudaosman/static/js/runtime-main.8de8455b.js"
+  },
+  {
+    "revision": "8e454b00a3f516307e31bb1dd9de296b",
+    "url": "/mahmoudaosman/static/media/bachelor02.8e454b00.png"
+  },
+  {
+    "revision": "b044d021430fa26f32ffe19260ed33d6",
+    "url": "/mahmoudaosman/static/media/ccv02.b044d021.png"
+  },
+  {
+    "revision": "29c6344f57a976121c0000070f476597",
+    "url": "/mahmoudaosman/static/media/certs02.29c6344f.png"
+  }
+]);
