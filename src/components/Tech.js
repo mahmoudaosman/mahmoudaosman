0141f0c8a10e9f8d618d8a3b7cf5493171d52dca
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-04-12 20:29:43 -0500

    added built with section

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -6,17 +6,19 @@ export class Tech extends Component {
     return (
       <div className="built-with-react">
         <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <h2>Built with:</h2>
-            </div>
+          <div className="built-with-items">
+            <div className="row">
+              <div className="col-md-6">
+                <h2>Built with:</h2>
+              </div>
 
-            <div className="col-md-6">
-              <img
-                src={ReactLogo}
-                className="img-responsive"
-                alt="React Logo"
-              />
+              <div className="col-md-6">
+                <img
+                  src={ReactLogo}
+                  className="img-responsive"
+                  alt="React Logo"
+                />
+              </div>
             </div>
           </div>
         </div>
